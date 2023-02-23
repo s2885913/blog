308166736ef8e1201dc642abc70df8b215447b90
lzr <1598042226@qq.com>
2023-02-24 01:51:43 +0800

    表格优化

--- blog-admin/src/views/article/writeArticle/index.vue
+++ blog-admin/src/views/article/writeArticle/index.vue
@@ -2,92 +2,107 @@
   <div class="container">
     <Breadcrumb :items="['menu.article', 'menu.article.write']" />
     <div class="content">
-      <a-form :model="form" label-align="right" :auto-label-width="true">
-        <a-row :gutter="0" justify="space-around">
-          <a-col :span="7">
-            <a-form-item field="title" label="文章标题" label-col-flex="100px">
-              <a-input
-                v-model="form.title"
-                size="large"
-                placeholder="请输入文章标题"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col :span="5">
-            <a-form-item
-              field="classify"
-              label="文章分类"
-              :rules="[{ match: /section one/, message: 'must select one' }]"
-            >
-              <a-select
-                v-model="form.classify"
-                placeholder="请选择"
-                size="large"
+      <div class="body">
+        <a-form :model="form" label-align="right" :auto-label-width="true">
+          <a-row :gutter="0" justify="start">
+            <a-col :span="7">
+              <a-form-item field="title" label="文章标题">
+                <a-input
+                  v-model="form.title"
+                  size="large"
+                  placeholder="请输入文章标题"
+                />
+              </a-form-item>
+            </a-col>
+            <a-col :span="7">
+              <a-form-item
+                field="classify"
+                label="文章分类"
+                label-col-flex="100px"
               >
-                <a-option value="section one">Section One</a-option>
-                <a-option value="section two">Section Two</a-option>
-                <a-option value="section three">Section Three</a-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
-          <a-col :span="7">
-            <a-form-item field="tag" label="文章标签">
-              <a-select v-model="form.tag" placeholder="请选择" multiple>
-                <a-option value="section one">Section One</a-option>
-                <a-option value="section two">Section Two</a-option>
-                <a-option value="section three">Section Three</a-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
-        </a-row>
-        <a-row :gutter="0" justify="space-around">
-          <a-col :span="7">
-            <a-form-item field="title" label="缩略图" label-col-flex="100px">
-              <a-upload action="/">
-                <template #upload-button>
-                  <div class="upload">
-                    <div style="height: 38%; margin-top: -20px"
-                      ><icon-upload :size="35"
-                    /></div>
-                    <div style="height: 10%; margin-top: -8px"
-                      >将文件拖到此处或
-                      <span style="color: #3370ff">点击上传</span></div
-                    >
-                  </div>
-                </template>
-              </a-upload>
-            </a-form-item>
-          </a-col>
-          <a-col :span="5">
-            <a-form-item field="isComment" label="是否开启评论">
-              <a-radio-group v-model="form.isComment" size="large">
-                <a-radio value="1">是</a-radio>
-                <a-radio value="0">否</a-radio>
-              </a-radio-group>
-            </a-form-item>
-            <a-form-item>
-              <a-button type="primary" @click="preserve">
-                <template #icon><icon-save /></template> 保存到草稿箱</a-button
+                <a-select
+                  v-model="form.classify"
+                  placeholder="请选择"
+                  size="large"
+                >
+                  <a-option value="section one">Section One</a-option>
+                  <a-option value="section two">Section Two</a-option>
+                  <a-option value="section three">Section Three</a-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+            <a-col :span="7">
+              <a-form-item field="tag" label-col-flex="100px" label="文章标签">
+                <a-select v-model="form.tag" placeholder="请选择" multiple>
+                  <a-option value="section one">Section One</a-option>
+                  <a-option value="section two">Section Two</a-option>
+                  <a-option value="section three">Section Three</a-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="0" justify="start">
+            <a-col :span="7">
+              <a-form-item field="title" label="缩略图">
+                <a-upload action="/">
+                  <template #upload-button>
+                    <div class="upload">
+                      <div style="height: 38%; margin-top: -20px"
+                        ><icon-upload :size="35"
+                      /></div>
+                      <div style="height: 10%; margin-top: -8px"
+                        >将文件拖到此处或
+                        <span style="color: #3370ff">点击上传</span></div
+                      >
+                    </div>
+                  </template>
+                </a-upload>
+              </a-form-item>
+            </a-col>
+            <a-col :span="7" style="display: flex; flex-direction: row">
+              <a-form-item
+                field="isComment"
+                label-col-flex="100px"
+                label="开启评论"
               >
-            </a-form-item>
-          </a-col>
-          <a-col :span="7">
-            <a-form-item field="isTop" label="是否文章置顶">
-              <a-radio-group v-model="form.isTop" size="large">
-                <a-radio value="1">是</a-radio>
-                <a-radio value="0">否</a-radio>
-              </a-radio-group>
-            </a-form-item>
-            <a-form-item>
-              <a-button type="primary" @click="publish">
-                <template #icon><icon-send /></template>发布</a-button
+                <a-switch
+                  v-model="form.isComment"
+                  checked-color="#00B42A"
+                  checked-value="1"
+                  unchecked-value="0"
+                />
+              </a-form-item>
+              <a-form-item
+                field="isTop"
+                label-col-flex="100px"
+                label="文章置顶"
               >
-            </a-form-item>
-          </a-col>
-        </a-row>
-      </a-form>
-      <div class="ed">
-        <md-editor v-model="previewData" />
+                <a-switch
+                  v-model="form.isTop"
+                  checked-color="#00B42A"
+                  checked-value="1"
+                  unchecked-value="0"
+                />
+              </a-form-item>
+            </a-col>
+            <a-col :span="7" style="display: flex; flex-direction: row">
+              <a-form-item label-col-flex="100px">
+                <a-button type="outline" @click="preserve">
+                  <template #icon><icon-save /></template>
+                  保存</a-button
+                >
+              </a-form-item>
+              <a-form-item>
+                <a-button type="outline" @click="publish">
+                  <template #icon><icon-send /></template>发布</a-button
+                >
+              </a-form-item>
+            </a-col>
+          </a-row>
+        </a-form>
+        <div class="ed">
+          <md-editor v-model="previewData" />
+        </div>
       </div>
     </div>
   </div>
@@ -143,5 +158,9 @@
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
+  }
+
+  .arco-switch {
+    border-radius: 12px !important;
   }
 </style>
--- blog-admin/src/views/system/user/index.vue
+++ blog-admin/src/views/system/user/index.vue
@@ -3,92 +3,119 @@
     <div class="container">
       <Breadcrumb :items="['menu.system', 'menu.system.user']" />
       <div class="content">
-        <div class="search">
-          <a-row :gutter="16">
-            <a-col :span="5">
-              <a-form-item
-                field="username"
-                label="用户名"
-                label-col-flex="70px"
-              >
-                <a-input v-model="form.username" placeholder="请输入用户名" />
-              </a-form-item>
-            </a-col>
-            <a-col :span="5">
-              <a-form-item field="value2" label="邮箱" label-col-flex="50px">
-                <a-input v-model="form.email" placeholder="请输入邮箱" />
-              </a-form-item>
-            </a-col>
-            <a-col :span="5">
-              <a-form-item field="status" label="状态" label-col-flex="50px">
-                <a-select v-model="form.status" placeholder="请选择状态">
-                  <a-option value="0">已禁用</a-option>
-                  <a-option value="1">未禁用</a-option>
-                </a-select>
-              </a-form-item>
-            </a-col>
-            <a-col :span="6">
-              <a-form-item>
-                <a-button type="primary" @click="search"
-                  ><template #icon><icon-search /></template>搜索</a-button
-                >
-              </a-form-item>
-            </a-col>
-          </a-row>
-        </div>
-
-        <div class="user-content">
-          <div class="bt">
+        <div class="body">
+          <div class="search">
             <a-row :gutter="16">
-              <a-space :size="-5">
-                <a-col :span="1">
-                  <a-form-item label-col-flex="10px">
-                    <a-button type="primary" status="success" @click="addUser">
+              <a-col :span="7">
+                <a-form-item field="username" label="用户名">
+                  <a-input
+                    v-model="form.username"
+                    size="large"
+                    placeholder="请输入用户名"
+                  />
+                </a-form-item>
+              </a-col>
+              <a-col :span="7">
+                <a-form-item field="value2" label="邮箱" label-col-flex="50px">
+                  <a-input
+                    v-model="form.email"
+                    size="large"
+                    placeholder="请输入邮箱"
+                  />
+                </a-form-item>
+              </a-col>
+              <a-col :span="7">
+                <a-form-item field="status" label="状态" label-col-flex="50px">
+                  <a-select
+                    v-model="form.status"
+                    size="large"
+                    placeholder="请选择状态"
+                  >
+                    <a-option value="0">已启用</a-option>
+                    <a-option value="1">未启用</a-option>
+                  </a-select>
+                </a-form-item>
+              </a-col>
+              <a-col :span="2">
+                <a-form-item>
+                  <a-button type="outline" @click="search"
+                    ><template #icon><icon-search /></template>搜索</a-button
+                  >
+                </a-form-item>
+              </a-col>
+            </a-row>
+          </div>
+          <div class="user-content">
+            <a-divider style="margin-top: 0" />
+            <div class="bt">
+              <a-row justify="start">
+                <a-col :span="2">
+                  <a-form-item :no-style="true">
+                    <a-button type="outline" status="success" @click="addUser">
                       <template #icon><icon-user-add /></template>
                       新增</a-button
                     >
                   </a-form-item>
                 </a-col>
-                <a-col :span="1">
-                  <a-form-item label-col-flex="0px">
-                    <a-button type="primary" status="danger" @click="deleteUser"
-                      ><template #icon><icon-delete /></template>删除</a-button
-                    >
+                <a-col :span="2">
+                  <a-form-item :no-style="true">
+                    <a-popconfirm content="你确定要删除吗?">
+                      <a-button
+                        type="outline"
+                        status="danger"
+                        @click="deleteUser"
+                        ><template #icon><icon-delete /></template
+                        >删除</a-button
+                      >
+                    </a-popconfirm>
                   </a-form-item>
                 </a-col>
-              </a-space>
-            </a-row>
+              </a-row>
+            </div>
           </div>
+          <a-table
+            row-key="id"
+            :columns="columns"
+            :data="data"
+            :table-layout-fixed="true"
+            :pagination="true"
+            page-position="br"
+            :bordered="false"
+            :hoverable="true"
+            :stripe="false"
+            :loading="false"
+            :show-header="true"
+            :row-selection="{ type: 'checkbox', showCheckedAll: true }"
+          >
+            <template #status="{ record }"
+              ><a-switch
+                v-model="record.status"
+                checked-color="#00B42A"
+                checked-value="1"
+                unchecked-value="0"
+            /></template>
+            <template #operate
+              ><a-button-group type="outline">
+                <a-space>
+                  <a-button>
+                    <template #icon><icon-edit /></template>
+                  </a-button>
+                  <a-popconfirm content="你确定要删除吗?">
+                    <a-button status="danger">
+                      <template #icon><icon-delete /></template>
+                    </a-button>
+                  </a-popconfirm>
+                </a-space> </a-button-group
+            ></template>
+          </a-table>
         </div>
-        <a-table
-          row-key="id"
-          :columns="columns"
-          :data="data"
-          :table-layout-fixed="true"
-          :pagination="true"
-          page-position="br"
-          :bordered="false"
-          :hoverable="true"
-          :stripe="false"
-          :loading="false"
-          :show-header="true"
-          :row-selection="rowSelection"
-        >
-          <template #status="{ record }"
-            ><a-switch
-              v-model="record.status"
-              checked-color="#00B42A"
-              checked-value="1"
-              unchecked-value="0"
-          /></template>
-        </a-table>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { reactive, ref } from 'vue';
+  import { reactive } from 'vue';
 
   //   const expandedKeys = ref([]);
 
@@ -98,26 +125,22 @@
     status: '',
   });
 
-  const selectedKeys = ref(['Jane Doe', 'Alisa Ross']);
+  //   const selectedKeys = ref(['Jane Doe', 'Alisa Ross']);
 
-  const rowSelection = reactive({
-    type: 'checkbox',
-    showCheckedAll: true,
-  });
   const columns = [
-    { title: '用户编号', dataIndex: 'id' },
-    { title: '用户名', dataIndex: 'username' },
-    { title: '邮箱', dataIndex: 'email' },
-    { title: '创建时间', dataIndex: 'createTime' },
-    { title: '状态', slotName: 'status' },
-    { title: '操作', dataIndex: 'operate' },
+    { title: '用户编号', dataIndex: 'id', align: 'center' },
+    { title: '用户名', dataIndex: 'username', align: 'center' },
+    { title: '邮箱', dataIndex: 'email', align: 'center' },
+    { title: '创建时间', dataIndex: 'createTime', align: 'center' },
+    { title: '状态', slotName: 'status', align: 'center' },
+    { title: '操作', slotName: 'operate', align: 'center' },
   ];
 
   const data = [
     {
       id: '1',
-      username: 'Jane Doe',
-      email: 'jane.doe@example.com',
+      username: 'Quinlan',
+      email: '1598042226@qq.com',
       createTime: '2022-10-20 24:23:22',
     },
     {
@@ -142,7 +165,12 @@
   thead {
     background-color: #fff !important;
   }
-  .user-content {
-    margin-top: 20px;
+
+  .bt {
+    margin-bottom: 10px;
+  }
+
+  .arco-table-td-content .arco-switch {
+    border-radius: 12px !important;
   }
 </style>
