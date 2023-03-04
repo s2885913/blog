85126f8757bbc574da3ab647457a41986386cf1a
1176827428@qq.com <1176827428@qq.com>
2023-03-04 12:03:12 +0800

    留言页面完毕, 但是弹幕有点卡, 需要优化

--- blog/src/views/message/index.vue
+++ blog/src/views/message/index.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { ref } from 'vue'
+import Comment from '@/components/layout/Comment.vue'
 import Message from '@/components/layout/Message.vue'
 
 let idNum = ref(0)
 let inputContent = ref('')
 let img1 = ref('/src/assets/img/picture/img1.jpeg')
+let img4 = ref('/src/assets/img/picture/cover.png')
+
 const list = reactive([
     {
         id: 0,
@@ -151,6 +154,41 @@ function addList() {
                 <Message :img="item.img" :content="item.content" :delay="item.delay"></Message>
             </template>
         </main>
+
+        <div class="comment-content">
+            <div class="commentHead">
+                <icon-pen-fill />
+                留言
+            </div>
+            <div class="writeComment">
+                <textarea data-v-f4b95078="" placeholder="写下点什么..." maxlength="1000" class="comment-textarea"></textarea>
+                <div class="myBetween">
+                    <div class="left">
+                        <icon-face-smile-fill />
+                        <icon-image />
+                    </div>
+                    <div class="right">
+                        <div class="myButton">提交</div>
+                    </div>
+                </div>
+            </div>
+            <div class="commentInfo">
+                <div class="commentInfo-title">
+                    <span style="font-size: 1.15rem">Comments | </span>
+                    <span>6 条留言</span>
+                </div>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+                <Comment :img="img4" nickname="Sara" time="2023-01-08 17:53" content="提前谢谢大家的祝福!"></Comment>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -199,5 +237,70 @@ button {
     padding: 10px 10px;
     margin-left: 10px;
     cursor: pointer;
+}
+
+.comment-content {
+    max-width: 1000px;
+    width: 1000px;
+    margin: 0 auto;
+}
+
+.commentHead {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    font-weight: 700;
+    margin: 40px 0 20px 0;
+    color: var(--themeBackground);
+}
+
+.writeComment {
+    margin-bottom: 40px;
+}
+
+.writeComment .comment-textarea {
+    border: 1px solid var(--lightGray);
+    width: 100%;
+    font-size: 14px;
+    padding: 15px;
+    min-height: 180px;
+    resize: none;
+    outline: none;
+    border-radius: 4px;
+    background-image: var(--commentURL);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: 100%;
+    margin-bottom: 10px;
+}
+
+.myBetween {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.myBetween .left {
+    width: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 20px;
+}
+
+.myButton {
+    background: rgb(131, 123, 199);
+    width: 66px;
+    height: 33px;
+    line-height: 33px;
+    border-radius: 4px;
+    text-align: center;
+    color: white;
+}
+
+.commentInfo-title {
+    margin-bottom: 20px;
+    color: var(--greyFont);
 }
 </style>
